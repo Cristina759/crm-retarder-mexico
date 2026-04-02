daade669c9c77dccd6d22c39534479c80a9f9f0e
Retarder Mexico Dev <dev@retardermexico.com>
2026-04-02 05:54:44 -0600

    fix: dashboard total fix

---
+++ fix2.js
@@ -0,0 +1,1 @@
+const fs=require('fs');const p='src/app/(dashboard)/dashboard/page.tsx';let c=fs.readFileSync(p,'utf8');c=c.replace(/current[.]monto/g,'current.total');c=c.replace(/o[.]monto/g,'o.total');fs.writeFileSync(p,c,'utf8');console.log('OK');
